011f6a160b361d73d0337d6fb6f79f0fb8d0e5e7
Danny <byxdenpro@gmail.com>
2024-07-02 10:18:44 +0300

    Use Ajax to fetch information from API

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-    
-}); 
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/todos",
+        type: "GET",
+        cache: false,
+        success: function (data) {
+            $.each(data, function(key, value) {
+                var row = $('<tr></tr>');
+                var tableBody = $('#table');
+                row.append('<td>' + value.userID + '</td>');
+                row.append('<td>' + value.id + '</td>');
+                row.append('<td>' + value.title + '</td>');
+                row.append('<td>' + value.completed + '</td>');
+                tableBody.append(row);
+            });
+        }
+    }); 
+});
